1d1af6e4e3ca599df93619ddd10ee513a2773b98
Sam Argy <samargy@Sams-MacBook-Pro.local>
2019-03-06 15:16:46 +1100

    First Commit :D

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const mongodb = require('mongodb').MongoClient;
+
+//conneting mongo url
+var url = 'mongodb://localhost:27017';
+
+//db name 
+var dbName = 'testapp';
+
+//Connecting to server
+mongodb.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+  
+  insertDocuments(db, function() {
+    client.close();
+  });
+});
+
+//inserting documents for mongo
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
